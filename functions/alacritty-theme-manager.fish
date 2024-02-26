f686905e9a07900284df269e230edc92cff4feb0
jnkrm <jo.nykrem@mf.me>
2024-02-26 19:43:58 +0100

    minor fix

--- functions/alacritty-theme-manager.fish
+++ functions/alacritty-theme-manager.fish
@@ -1,5 +1,5 @@
 # Function to manage themes in the Alacritty terminal emulator
-function alacritty-theme-manager -d "Manage Alacritty themes"
+function atm -d "Manage Alacritty themes"
     # All themes
     echo $(string match -gr "(.*)\.toml" $(ls ~/.config/alacritty/themes/themes))|read -a themes_all
     # Starred themes
@@ -50,7 +50,7 @@ function alacritty-theme-manager -d "Manage Alacritty themes"
             echo $theme_random >> ~/.config/alacritty/theme_current
         case 1
             # Print all themes
-            if test $argv = ls
+            if test $argv = l
                 set counter 0
                 for theme in $themes_all
                     set counter (math $counter + 1)
@@ -63,7 +63,7 @@ function alacritty-theme-manager -d "Manage Alacritty themes"
                     end
                 end
             # Star current theme
-            else if test $argv = star
+            else if test $argv = s
                 if not contains $theme_current $themes_starred
                     echo $theme_current >> ~/.config/alacritty/themes_starred
                     echo -e "\e[1;3m$theme_current\e[0m successfully added to "\uf005\uf005\uf005
@@ -71,14 +71,14 @@ function alacritty-theme-manager -d "Manage Alacritty themes"
                     echo -e "\e[1;3m$theme_current\e[0m is already in "\uf005\uf005\uf005
                 end
             # Get info about current theme
-            else if test $argv = info
+            else if test $argv = i
                 if contains $theme_current $themes_starred
                     echo -e "Current theme: \e[1;3m$theme_current\e[0m" \uf005
                 else
                     echo -e "Current theme: \e[1;3m$theme_current\e[0m"
                 end
                 # Shuffle through a set of random themes
-            else if test $argv = random
+            else if test $argv = r
                 set counter 0
                 while test $counter -lt 10
                     set counter (math $counter + 1)
@@ -95,6 +95,17 @@ function alacritty-theme-manager -d "Manage Alacritty themes"
                 end
                 echo -n > ~/.config/alacritty/theme_current
                 echo $theme_random >> ~/.config/alacritty/theme_current
+            else if test $argv = u
+                if contains $theme_current $themes_starred
+                    set themes_starred (string match -v $theme_current $themes_starred)
+                    echo -n > ~/.config/alacritty/themes_starred
+                    for theme_starred in $themes_starred
+                        echo $theme_starred >> ~/.config/alacritty/themes_starred
+                    end
+                    echo -e "Successfully removed \e[1;3m$theme_current\e[0m from "\uf005\uf005\uf005
+                else
+                    echo -e "\e[1;3m$theme_current\e[0m is not a starred theme, so it cannot be removed from "\uf005\uf005\uf005
+                end
             else
                 # 
                 if contains $argv $themes_all
@@ -112,15 +123,15 @@ function alacritty-theme-manager -d "Manage Alacritty themes"
                 end
             end
         case 2
-            if test $argv[1] = star && test $argv[2] = ls
+            if test $argv[1] = s && test $argv[2] = s
                 for theme in $themes_starred
                     echo -e \uf005 "\e[1;3m$theme\e[0m"
                 end
-            else if test $argv[1] = ls && test $argv[2] = star
+            else if test $argv[1] = s && test $argv[2] = s
                 for theme in $themes_starred
                     echo -e \uf005 "\e[1;3m$theme\e[0m"
                 end
-            else if test $argv[1] = star
+            else if test $argv[1] = s
                 set theme $argv[2]
                 if contains $theme $themes_all
                     if not contains $theme $themes_starred
@@ -132,7 +143,7 @@ function alacritty-theme-manager -d "Manage Alacritty themes"
                 else
                     echo -e "\e[1;3m$theme\e[0m is \e[4mnot\e[0m a valid theme name.\nCheck available theme names with the function \e[1;3mlist_themes\e[0m"
                 end
-            else if test $argv[1] = rm
+            else if test $argv[1] = u
                 set theme $argv[2]
                 if contains $theme $themes_all
                     if contains $theme $themes_starred
