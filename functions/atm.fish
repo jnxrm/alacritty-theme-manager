f4cd20de3a9dc47145eec91d912fdd7f7620eb79
jnkrm <jo.nykrem@mf.me>
2024-02-27 01:49:05 +0100

    replaced ~/.config/alacritty by CONFIG

--- functions/atm.fish
+++ functions/atm.fish
@@ -1,21 +1,22 @@
 # Function to manage themes in the Alacritty terminal emulator
 function atm -d "alacritty-theme-manager"
+    set CONFIG ~/.config/alacritty
     # All themes
-    echo $(string match -gr "(.*)\.toml" $(ls ~/.config/alacritty/themes/themes))|read -a themes_all
+    echo $(string match -gr "(.*)\.toml" $(ls $CONFIG/themes/themes))|read -a themes_all
     # Starred themes
-    if not test -e ~/.config/alacritty/themes_starred
-        echo $(touch ~/.config/alacritty/themes_starred)
+    if not test -e $CONFIG/themes_starred
+        echo $(touch $CONFIG/themes_starred)
     end
-    echo $(cat ~/.config/alacritty/themes_starred)|read -a themes_starred
+    echo $(cat $CONFIG/themes_starred)|read -a themes_starred
     # Light themes
-    if not test -e ~/.config/alacritty/themes_light
-        echo $(touch ~/.config/alacritty/themes_light)
+    if not test -e $CONFIG/themes_light
+        echo $(touch $CONFIG/themes_light)
         set themes_light alabaster ashes_light atom_one_light ayu_light catppuccin_latte everforest_light github_light github_light_colorblind github_light_default github_light_high_contrast github_light_tritanopia gruvbox_light gruvbox_material_hard_light gruvbox_material_medium_light night_owlish_light noctis-lux nord_light papercolor_light papertheme pencil_light rose-pine-dawn solarized_light
         for theme in $themes_light
-            echo $theme >> ~/.config/alacritty/themes_light
+            echo $theme >> $CONFIG/themes_light
         end
     end
-    echo $(cat ~/.config/alacritty/themes_light)|read -a themes_light
+    echo $(cat $CONFIG/themes_light)|read -a themes_light
     # Dark themes
     set themes_dark
     for theme in $themes_all
@@ -24,11 +25,11 @@ function atm -d "alacritty-theme-manager"
         end
     end
     # Current theme
-    if not test -e ~/.config/alacritty/theme_current
-        echo $(touch ~/.config/alacritty/theme_current)
-        echo papercolor_dark >> ~/.config/alacritty/theme_current
+    if not test -e $CONFIG/theme_current
+        echo $(touch $CONFIG/theme_current)
+        echo papercolor_dark >> $CONFIG/theme_current
     end
-    set theme_current $(cat ~/.config/alacritty/theme_current)
+    set theme_current $(cat $CONFIG/theme_current)
     switch $(count $argv)
         case 0
             # Shuffle through a set of random themes
@@ -36,8 +37,8 @@ function atm -d "alacritty-theme-manager"
             while test $counter -lt 10
                 set counter (math $counter + 1)
                 set theme_random $(random choice $themes_dark)
-                cp ~/.config/alacritty/themes/themes/$theme_random.toml ~/.config/alacritty/
-                mv ~/.config/alacritty/$theme_random.toml ~/.config/alacritty/theme.toml
+                cp $CONFIG/themes/themes/$theme_random.toml $CONFIG/
+                mv $CONFIG/$theme_random.toml $CONFIG/theme.toml
                 sleep 0.05
             end
             # Echo theme name in italic using escape characters
@@ -46,8 +47,8 @@ function atm -d "alacritty-theme-manager"
             else
                 echo -e "Theme changed to: \e[1;3m$theme_random\e[0m"
             end
-            echo -n > ~/.config/alacritty/theme_current
-            echo $theme_random >> ~/.config/alacritty/theme_current
+            echo -n > $CONFIG/theme_current
+            echo $theme_random >> $CONFIG/theme_current
         case 1
             # Print all themes
             if test $argv = ls
@@ -65,7 +66,7 @@ function atm -d "alacritty-theme-manager"
             # Star current theme
             else if test $argv = s
                 if not contains $theme_current $themes_starred
-                    echo $theme_current >> ~/.config/alacritty/themes_starred
+                    echo $theme_current >> $CONFIG/themes_starred
                     echo -e "\e[1;3m$theme_current\e[0m successfully added to "\uf005\uf005\uf005
                 else
                     echo -e "\e[1;3m$theme_current\e[0m is already in "\uf005\uf005\uf005
@@ -83,8 +84,8 @@ function atm -d "alacritty-theme-manager"
                 while test $counter -lt 10
                     set counter (math $counter + 1)
                     set theme_random $(random choice $themes_dark)
-                    cp ~/.config/alacritty/themes/themes/$theme_random.toml ~/.config/alacritty/
-                    mv ~/.config/alacritty/$theme_random.toml ~/.config/alacritty/theme.toml
+                    cp $CONFIG/themes/themes/$theme_random.toml $CONFIG/
+                    mv $CONFIG/$theme_random.toml $CONFIG/theme.toml
                     sleep 0.05
                 end
                 # Echo theme name in italic using escape characters
@@ -93,14 +94,14 @@ function atm -d "alacritty-theme-manager"
                 else
                     echo -e "Theme changed to: \e[1;3m$theme_random\e[0m"
                 end
-                echo -n > ~/.config/alacritty/theme_current
-                echo $theme_random >> ~/.config/alacritty/theme_current
+                echo -n > $CONFIG/theme_current
+                echo $theme_random >> $CONFIG/theme_current
             else if test $argv = u
                 if contains $theme_current $themes_starred
                     set themes_starred (string match -v $theme_current $themes_starred)
-                    echo -n > ~/.config/alacritty/themes_starred
+                    echo -n > $CONFIG/themes_starred
                     for theme_starred in $themes_starred
-                        echo $theme_starred >> ~/.config/alacritty/themes_starred
+                        echo $theme_starred >> $CONFIG/themes_starred
                     end
                     echo -e "Successfully removed \e[1;3m$theme_current\e[0m from "\uf005\uf005\uf005
                 else
@@ -109,10 +110,10 @@ function atm -d "alacritty-theme-manager"
             else
                 # 
                 if contains $argv $themes_all
-                    cp ~/.config/alacritty/themes/themes/$argv.toml ~/.config/alacritty/
-                    mv ~/.config/alacritty/$argv.toml ~/.config/alacritty/theme.toml
-                    echo -n > ~/.config/alacritty/theme_current
-                    echo $argv >> ~/.config/alacritty/theme_current
+                    cp $CONFIG/themes/themes/$argv.toml $CONFIG/
+                    mv $CONFIG/$argv.toml $CONFIG/theme.toml
+                    echo -n > $CONFIG/theme_current
+                    echo $argv >> $CONFIG/theme_current
                     if contains $argv $themes_starred
                         echo -e "Current theme: \e[1;3m$argv\e[0m" \uf005
                     else
@@ -135,7 +136,7 @@ function atm -d "alacritty-theme-manager"
                 set theme $argv[2]
                 if contains $theme $themes_all
                     if not contains $theme $themes_starred
-                        echo $theme >> ~/.config/alacritty/themes_starred
+                        echo $theme >> $CONFIG/themes_starred
                         echo -e "\e[1;3m$theme\e[0m successfully added to "\uf005\uf005\uf005
                     else
                         echo -e "\e[1;3m$theme\e[0m is already in "\uf005\uf005\uf005
@@ -148,9 +149,9 @@ function atm -d "alacritty-theme-manager"
                 if contains $theme $themes_all
                     if contains $theme $themes_starred
                         set themes_starred (string match -v $theme $themes_starred)
-                        echo -n > ~/.config/alacritty/themes_starred
+                        echo -n > $CONFIG/themes_starred
                         for theme_starred in $themes_starred
-                            echo $theme_starred >> ~/.config/alacritty/themes_starred
+                            echo $theme_starred >> $CONFIG/themes_starred
                         end
                         echo -e "Successfully removed \e[1;3m$theme\e[0m from "\uf005\uf005\uf005
                     else
