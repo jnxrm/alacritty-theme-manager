13b4d0c69c6dcd996ac12c5cdc5190c19d5196a7
jnxrm <jo.nykrem@mf.me>
2024-02-27 17:28:00 +0100

    q

--- functions/atm.fish
+++ functions/atm.fish
@@ -1,11 +1,9 @@
 # Function to manage themes in the Alacritty terminal emulator
 function atm -d "alacritty-theme-manager"
-    # atm-themes directory
+    # atm and Alacritty directories
     set fisher_path $__fish_config_dir
     set atm_path $fisher_path/functions/atm
-
-    # Alacritty directories
-    set ala_path ~/.config/alacritty/atm
+    set ala_path ~/.config/alacritty
     if not test -d $ala_path
         echo $(mkdir $ala_path)
     end
@@ -15,10 +13,10 @@ function atm -d "alacritty-theme-manager"
     echo $(string match -gr "(.*)\.toml" $(ls $ala_themes))|read -a themes_all
 
     # Starred themes
-    if not test -e $ala_path/themes_starred
-        echo $(touch $ala_path/themes_starred)
+    if not test -e $ala_path/atm/themes_starred
+        echo $(touch $ala_path/atm/themes_starred)
     end
-    echo $(cat $ala_path/themes_starred)|read -a themes_starred
+    echo $(cat $ala_path/atm/themes_starred)|read -a themes_starred
 
     # Light themes
     echo $(cat $atm_path/themes_light)|read -a themes_light
@@ -32,12 +30,12 @@ function atm -d "alacritty-theme-manager"
     end
 
     # Current theme
-    if not test -e $ala_path/theme_current
-        echo $(touch $ala_path/theme_current)
-        echo base16_default_dark >> $ala_path/theme_current
+    if not test -e $ala_path/atm/theme_current
+        echo $(touch $ala_path/atm/theme_current)
+        echo base16_default_dark >> $ala_path/atm/theme_current
     else
-        if contains $(cat $ala_path/theme_current) $themes_all
-            set theme_current $(cat $ala_path/theme_current)
+        if contains $(cat $ala_path/atm/theme_current) $themes_all
+            set theme_current $(cat $ala_path/atm/theme_current)
         else
             set theme_current base16_default_dark
         end
@@ -51,7 +49,7 @@ function atm -d "alacritty-theme-manager"
                     # Star current theme
                     case 1
                         if not contains $theme_current $themes_starred
-                            echo $theme_current >> $ala_path/themes_starred
+                            echo $theme_current >> $ala_path/atm/themes_starred
                             echo -e "\e[1;3m$theme_current\e[0m successfully added to "\uf005\uf005\uf005
                         else
                             echo -e "\e[1;3m$theme_current\e[0m is already in "\uf005\uf005\uf005
@@ -67,7 +65,7 @@ function atm -d "alacritty-theme-manager"
                             set theme $argv[2]
                             if contains $theme $themes_all
                                 if not contains $theme $themes_starred
-                                    echo $theme >> $ala_path/themes_starred
+                                    echo $theme >> $ala_path/atm/themes_starred
                                     echo -e "\e[1;3m$theme\e[0m successfully added to "\uf005\uf005\uf005
                                 else
                                     echo -e "\e[1;3m$theme\e[0m is already in "\uf005\uf005\uf005
@@ -87,9 +85,9 @@ function atm -d "alacritty-theme-manager"
                     case 1
                         if contains $theme_current $themes_starred
                             set themes_starred (string match -v $theme_current $themes_starred)
-                            echo -n > $ala_path/themes_starred
+                            echo -n > $ala_path/atm/themes_starred
                             for theme_starred in $themes_starred
-                                echo $theme_starred >> $ala_path/themes_starred
+                                echo $theme_starred >> $ala_path/atm/themes_starred
                             end
                             echo -e "Successfully removed \e[1;3m$theme_current\e[0m from "\uf005\uf005\uf005
                         else
@@ -101,9 +99,9 @@ function atm -d "alacritty-theme-manager"
                         if contains $theme $themes_all
                             if contains $theme $themes_starred
                                 set themes_starred (string match -v $theme $themes_starred)
-                                echo -n > $ala_path/themes_starred
+                                echo -n > $ala_path/atm/themes_starred
                                 for theme_starred in $themes_starred
-                                    echo $theme_starred >> $ala_path/themes_starred
+                                    echo $theme_starred >> $ala_path/atm/themes_starred
                                 end
                                 echo -e "Successfully removed \e[1;3m$theme\e[0m from "\uf005\uf005\uf005
                             else
@@ -145,9 +143,9 @@ function atm -d "alacritty-theme-manager"
             case \*
                 if contains $argv $themes_all
                     cp $ala_themes/$argv.toml $ala_path
-                    mv $ala_path/$argv.toml $ala_path/theme.toml
-                    echo -n > $ala_path/theme_current
-                    echo $argv >> $ala_path/theme_current
+                    mv $ala_path/$argv.toml $ala_path/atm_theme.toml
+                    echo -n > $ala_path/atm/theme_current
+                    echo $argv >> $ala_path/atm/theme_current
                     if contains $argv $themes_starred
                         echo -e "Current theme: \e[1;3m$argv\e[0m" \uf005
                     else
@@ -164,7 +162,7 @@ function atm -d "alacritty-theme-manager"
             set counter (math $counter + 1)
             set theme_random $(random choice $themes_dark)
             cp $ala_themes/$theme_random.toml $ala_path
-            mv $ala_path/$theme_random.toml $ala_path/theme.toml
+            mv $ala_path/$theme_random.toml $ala_path/atm_theme.toml
             sleep 0.05
         end
         # Echo theme name in italic using escape characters
@@ -173,7 +171,7 @@ function atm -d "alacritty-theme-manager"
         else
             echo -e "Theme changed to: \e[1;3m$theme_random\e[0m"
         end
-        echo -n > $ala_path/theme_current
-        echo $theme_random >> $ala_path/theme_current
+        echo -n > $ala_path/atm/theme_current
+        echo $theme_random >> $ala_path/atm/theme_current
     end
 end
